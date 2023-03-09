048877b51edb4a02af2a1acdc4b96dae9da48edd
Milosska <110258611+Milosska@users.noreply.github.com>
2023-03-09 17:45:49 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import { GlobalStyles } from './GlobalStyles';
+import 'modern-normalize';
+
+import { StyledApp } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+  };
+
+  handleSubmit = ({ query }, { resetForm }) => {
+    this.setState({ query, page: 1 });
+    resetForm();
+  };
+
+  handleCilck = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  render() {
+    const { query, page } = this.state;
+    return (
+      <StyledApp>
+        <Searchbar onSubmit={this.handleSubmit} />
+        <ImageGallery
+          query={query}
+          page={page}
+          handleCilck={this.handleCilck}
+        />
+        <GlobalStyles />
+      </StyledApp>
+    );
+  }
+}
