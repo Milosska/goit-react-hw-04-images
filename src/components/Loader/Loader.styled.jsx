263b853e1778505998073f06f33626714baa83b5
Milosska <110258611+Milosska@users.noreply.github.com>
2023-03-10 22:52:30 +0200

    loader fixed

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Background = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: rgba(255, 255, 255, 0.7);
+  background-color: transparent;
 `;
